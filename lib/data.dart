69e9a98321060e4c521da57323ae385022d9d448
Noha-Teaima <nohateaima@gmail.com>
2023-08-25 11:50:23 +0200

    first commit

---
+++ lib/data.dart
@@ -0,0 +1,20 @@
+class data {
+  final String name;
+  final String creatAt;
+  final String message;
+  final String image;
+
+  data(
+      {required this.name,
+      required this.message,
+      required this.creatAt,
+      required this.image});
+}
+
+String img1 = ".dart_tool/image/img1.jfif";
+String img2 = ".dart_tool/image/img2.jfif";
+String img3 = ".dart_tool/image/img3.jfif";
+String img4 = ".dart_tool/image/img4.png";
+String img5 = ".dart_tool/image/img5.jfif";
+String img6 = ".dart_tool/image/img6.jfif";
+String img7 = ".dart_tool/image/img7.jfif";
