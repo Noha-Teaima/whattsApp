e595e77168cd513848772821a57f3b932b190614
Noha-Teaima <nohateaima@gmail.com>
2023-08-28 11:03:29 +0200

    BMI

--- lib/data.dart
+++ lib/data.dart
@@ -11,10 +11,10 @@ class data {
       required this.image});
 }
 
-String img1 = ".dart_tool/image/img1.jfif";
-String img2 = ".dart_tool/image/img2.jfif";
-String img3 = ".dart_tool/image/img3.jfif";
-String img4 = ".dart_tool/image/img4.png";
-String img5 = ".dart_tool/image/img5.jfif";
-String img6 = ".dart_tool/image/img6.jfif";
-String img7 = ".dart_tool/image/img7.jfif";
+String img1 = "lib/images/img1.jfif";
+String img2 = "lib/images/img2.jfif";
+String img3 = "lib/images/img3.jfif";
+String img4 = "lib/images/img5.jfif";
+String img5 = "lib/images/img6.jfif";
+String img6 = "lib/images/img7.jfif";
+String img7 = "lib/images/img4.png";
